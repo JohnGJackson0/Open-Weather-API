fc0841dcc560d79ac9b43c2edc20edddff0c09f3
jgjio <johngjackson0@gmail.com>
2021-09-25 17:23:49 -0400

    Add images to ForcastItems

--- src/Components/ForecastItem.tsx
+++ src/Components/ForecastItem.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { getFarenheight } from "../Conversion";
 
+//credits to so we don't have to make a million api calls
+//https://github.com/yuvraaaj/openweathermap-api-icons
 const ForecastItem = (props: any) => {
   return (
-    <View>
-      <Text>The Tempature: {getFarenheight(props.item.main.temp)}</Text>
-      <Text style={{ marginBottom: 5 }}>On Date: {props.item.dt_txt} </Text>
+    <View style={{ flexDirection: "row" }}>
+      <Image
+        style={{ width: 50, height: 50, resizeMode: "cover" }}
+        source={require("../icons/" + props.item.weather[0].icon + ".png")}
+      />
+      <Text>Temp: {getFarenheight(props.item.main.temp)}</Text>
+      <Text style={{ marginBottom: 5 }}> On Date: {props.item.dt_txt} </Text>
     </View>
   );
 };
